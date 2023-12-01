2d26287d87f2a63e721266384efc57d878a4ed89
Akash V <aakashvshetty@gmail.com>
2023-12-01 23:25:37 +0530

    assests are updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Admin from "./Components/Admin_UI";
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Components/Navbar'
+import Herosection from './Components/Herosection/index'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="mainbody">
-        <Admin />
-      </div>
+    <> 
+        <Navbar />
+        <Herosection />
     </>
   );
 }
